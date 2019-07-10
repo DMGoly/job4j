f0c29f0222c5fac7d49c63b52721f9cc1d12716e
Dmitry Golyshkin <dmgoly@gmail.com>
2019-07-10 19:37:56 +0300

    4. Используя шаблон проектирования - стратегию[#155013]

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
@@ -0,0 +1,11 @@
+package ru.job4j.pseudo;
+
+public class Paint {
+    public void draw(Shape shape){
+        System.out.println(shape.draw());
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
